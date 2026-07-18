1840de575de5c42cf3211583969a1ca40ff07ff6
udeshika-sewwandi <sewwandikaus9235@gmail.com>
2026-07-18 22:36:38 +1200

    add comments

--- src/main/java/org/sew/leetcode/PairSum.java
+++ src/main/java/org/sew/leetcode/PairSum.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class PairSum {
 
     public boolean findPair(int[] arr, int k) {
+        // Sort the array
         Arrays.sort(arr);
 
         int p = 0;
@@ -13,7 +14,14 @@ public class PairSum {
         return pair(p, q, arr, k);
     }
 
-
+    /**
+     * Recursive function to find the pair
+     * @param p left pointer
+     * @param q right pointer
+     * @param arr array
+     * @param k k
+     * @return if pair exists or not
+     */
     public boolean pair(int p, int q, int[] arr, int k) {
         if(p >= arr.length - 1 || q <= 0) return false;
 

6ba25b68d54f826b3f52d7c5f277c8b0e4f4c48b
udeshika-sewwandi <sewwandikaus9235@gmail.com>
2026-07-18 22:32:14 +1200

    Changes proceed

--- src/main/java/Problems.java
+++ src/main/java/Problems.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import org.sew.leetcode.PairSum;
 
 public class Problems {
+    public static void main(String[] args) {
+        PairSum pairsum = new PairSum();
+
+        int[] arr = {4, 5, 1, -3, 6};
+        System.out.println(pairsum.findPair(arr, 2));
+    }
 }
--- src/main/java/org/sew/leetcode/PairSum.java
+++ src/main/java/org/sew/leetcode/PairSum.java
@@ -1,4 +1,32 @@
 package org.sew.leetcode;
 
+import java.util.Arrays;
+
 public class PairSum {
+
+    public boolean findPair(int[] arr, int k) {
+        Arrays.sort(arr);
+
+        int p = 0;
+        int q = arr.length - 1;
+
+        return pair(p, q, arr, k);
+    }
+
+
+    public boolean pair(int p, int q, int[] arr, int k) {
+        if(p >= arr.length - 1 || q <= 0) return false;
+
+        if(arr[p] + arr[q] == k) {
+            return true;
+        }
+
+        if(arr[p] + arr[q] < k) {
+            p = p + 1;
+            return pair(p, q, arr, k);
+        }
+
+        q = q - 1;
+        return pair(p, q, arr, k);
+    }
 }
